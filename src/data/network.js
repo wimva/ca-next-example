e47c0ea10c7246185a5542185c3d0211db44fb6f
Wim Van Aerschot <wim@vaw.be>
2023-04-15 20:35:42 +0200

    http > https

--- src/data/network.js
+++ src/data/network.js
@@ -2,7 +2,7 @@ import fetcher from './_fetcher'
 import useSWR from 'swr'
 
 export default function useNetwork () {
-  const { data, error, isLoading } = useSWR(`http://api.citybik.es/v2/networks/velo-antwerpen`, fetcher)
+  const { data, error, isLoading } = useSWR(`https://api.citybik.es/v2/networks/velo-antwerpen`, fetcher)
  
   return {
     network: data?.network,
