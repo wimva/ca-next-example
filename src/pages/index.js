be9a4db1d2d72a06512d66c210eea5490fd9a59e
Wim Van Aerschot <wim@vaw.be>
2023-04-24 09:10:12 +0200

    sort stations by distance

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,6 +33,14 @@ export default function Home() {
 
   const stations = network.stations.filter(station => station.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0);
 
+  // map stations to add disrance to current location
+  stations.map(station => {
+    station.distance = getDistance(location.latitude, location.longitude, station.latitude, station.longitude).distance/1000;
+  });
+
+  // sort stations by distance
+  stations.sort((a, b) => a.distance - b.distance);
+
   function handleFilterChange(e) {
     setFilter(e.target.value);
   }
